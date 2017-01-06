2b12603473910e1bf29badc71edb733aae90f581
Hitman23 <mugtrade@gmail.com>
2017-01-06 10:41:14 +0300

    Update countwords.py

--- countwords.py
+++ countwords.py
@@ -2,21 +2,25 @@
 class stringCount:
 
     def countWords(self,x):
+        
         #define a dictionary to which we shall add the words and there values
         dictionary = {}
+        
         #get the length of the list provided
         num = len(x)
+        
         #iterating through the list provided as input
         iterator = 0
 
         while iterator<num:
             #seperate each word with in the current string. By default it is spaces
             words = x[iterator].split()
+            
             #loop through each of the separated words
             for k in words:
                 #convert each word to lower case
                 word = k.lower()
-                #check if the word has already been encountered and pushed into the dictionary
+                
                 if word in dictionary:
                     #if word already exists in the dictionary increments its value
                     dictionary[word]+=1
@@ -26,7 +30,6 @@ class stringCount:
             #increment the iteration factor(acting as our index) so as to access the next element in the sequence     
             iterator+=1
 
-        #return the dictionary
         return dictionary
 
 
@@ -34,10 +37,13 @@ class stringCount:
 wordstocount = ["hi there, how are you","are you coming for lunch tonight", "when are we going to the movies"]
 #a prompt for the user to enter his or her message
 newword = raw_input("Type your message here:")
+
 #add the user's message into our list
 wordstocount.append(newword)
+
 #creating object of the stringcount class
 returnedData = stringCount()
+
 #printing our output
 print returnedData.countWords(wordstocount)
 
